a077ba1cd6e8c0ab9a85421a40861321d631fcab
Masrur Sakib <mashrur333@gmail.com>
2020-03-02 21:29:29 +0600

    Update 02

---
+++ script.js
@@ -0,0 +1,68 @@
+// const mobilePrice = document.getElementById("mobile_price").innerText;
+// const mobilePriceNumber = parseFloat(mobilePrice);
+// const casingPrice = document.getElementById("casing_price").innerText;
+// const casingPriceNumber = parseFloat(casingPrice);
+
+const mobilePrice = 1219;
+const casingPrice = 59;
+
+const mobileQtyMinus = document.getElementById("mobile_qty_minus");
+mobileQtyMinus.addEventListener("click", function(){
+    minus_button_function("mobile_qty_input", mobilePrice, "mobile_price");
+    total_amount();
+    
+})
+
+const mobileQtyPlus = document.getElementById("mobile_qty_plus");
+mobileQtyPlus.addEventListener("click", function(){
+    plus_button_function("mobile_qty_input", mobilePrice, "mobile_price");
+    total_amount();
+})
+
+const casingQtyMinus = document.getElementById("casing_qty_minus");
+casingQtyMinus.addEventListener("click", function(){
+    minus_button_function("casing_qty_input", casingPrice, "casing_price");
+    total_amount();   
+})
+
+const casingQtyPlus = document.getElementById("casing_qty_plus");
+casingQtyPlus.addEventListener("click", function(){
+    plus_button_function("casing_qty_input", casingPrice, "casing_price");
+    total_amount();    
+})
+
+function minus_button_function(id, price, id1){
+    const currentQty = document.getElementById(id).value;
+    const currentQtyNumber = parseFloat(currentQty);
+    if (currentQtyNumber>=1) {
+        finalQtyNumber = currentQtyNumber-1;
+        document.getElementById(id).value = finalQtyNumber;
+
+        const finalMobilePrice = price*finalQtyNumber;
+        document.getElementById(id1).innerText = finalMobilePrice;
+        
+    }
+}
+
+function plus_button_function(id, price, id1){
+    const currentQty = document.getElementById(id).value;
+    const currentQtyNumber = parseFloat(currentQty);
+    const finalQtyNumber = currentQtyNumber+1;
+    document.getElementById(id).value = finalQtyNumber;
+
+    const finalCasingPrice = price*finalQtyNumber;
+    document.getElementById(id1).innerText = finalCasingPrice;
+
+}
+
+function total_amount(){
+    const subtotal_mobile_string = document.getElementById("mobile_price").innerText;
+    const subtotal_mobile = parseFloat(subtotal_mobile_string);
+    const subtotal_casing_string = document.getElementById("casing_price").innerText;
+    const subtotal_casing = parseFloat(subtotal_casing_string);
+    const subTotal = subtotal_mobile+subtotal_casing;
+    document.getElementById("subtotal").innerText = subTotal;
+    document.getElementById("tax").innerText = subTotal*0.01;
+}
+
+
